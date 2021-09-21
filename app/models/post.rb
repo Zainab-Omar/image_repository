26ba65abad4d2080a0f6070e869b4224e8289115
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-09-21 10:15:15 -0500

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+    validates :title, :image_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    
+    validates :username, :password, presence: true
+    validates :email, presence: true, uniqueness: true
+
 end
